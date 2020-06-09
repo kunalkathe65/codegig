5a009bdad14bdcd72c1335068b4308235d279010
kunalkathe65 <kunalkathe65@gmail.com>
2020-06-09 12:09:30 +0530

    added configs in env file

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('codegig', 'postgres', 'kunalkathe65', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorAliases: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+module.exports = new Sequelize(
+  process.env.DB,
+  process.env.DB_HOST,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorAliases: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
